f60cb674425c033dcfd54838a8b58842379fec26
James Rogers <jarogers095@gmail.com>
2020-04-01 04:04:52 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -8,7 +8,7 @@ class Actor < ActiveRecord::Base
   
   def list_roles
     self.characters.map do |char|
-      "#{char.name} - #{char.show}"
+      "#{char.name} - #{char.show.name}"
     end
   end
   
